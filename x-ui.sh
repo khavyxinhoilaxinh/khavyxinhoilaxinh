6033135c23cc7ed034ac29567d9d10999122e10f
khavyxinhoilaxinh <103309683+khavyxinhoilaxinh@users.noreply.github.com>
2022-04-09 16:46:16 +0700

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -2,8 +2,6 @@ apt update -y
 apt upgrade -y
 cd
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
-wget
-https://raw.githubusercontent.com/khavyxinhoilaxinh/khavyxinhoilaxinh/main/Remove-x-ui.sh
 clear
 apt install speedtest-cli -y
 echo -e "Bắt đầu kiểm tra tốc độ mạng"
@@ -14,5 +12,4 @@ echo -e "Kiểm tra hệ thống"
 neofetch
 echo -e "Kiểm tra địa chỉ ip"
 curl ipinfo.io
-echo -e "Nếu bạn muốn hủy cài đặt hãy nhập "bash Remove-x-ui.sh
 ##Copyright Kha Vy
