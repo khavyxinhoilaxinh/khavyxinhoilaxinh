8ed20481319b1a4cfd1689834599dfbf78c3beb3
khavyxinhoilaxinh <103309683+khavyxinhoilaxinh@users.noreply.github.com>
2022-04-17 00:36:57 +0700

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -2,11 +2,7 @@ apt update -y
 apt upgrade -y
 cd
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
-ufw allow 54321
-ufw allow 443
-ufw allow 80
-ufw allow 80/tcp
-wget https://raw.githubusercontent.com/khavyxinhoilaxinh/khavyxinhoilaxinh/main/huy-cai-dat.sh
+wget https://raw.githubusercontent.com/thangvoquyet46/thangvoquyet46/main/huy-cai-dat.sh
 clear
 apt install speedtest-cli -y
 echo -e "Bắt đầu kiểm tra tốc độ mạng"
@@ -18,4 +14,4 @@ neofetch
 echo -e "Kiểm tra địa chỉ ip"
 curl ipinfo.io
 echo -e "Nếu bạn muốn hủy cài đặt hãy nhập "bash huy-cai-dat.sh
-##Copyright Kha Vy
+##Copyright Kha Vy##
